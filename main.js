a953945d223b7bff5630f3cce7ce8cc7ddf0759e
Jesus David Molina <jesusdavidmolina.developer.com>
2023-04-25 21:57:27 -0500

    se agrega la validacion de los formularios

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
+const modal = document.getElementById('modal');
+const form  = document.getElementById('form');
+const buttonMensaje = document.getElementById('button');
+
+buttonMensaje.addEventListener('click', () => {
+    if(validacion == false){
+        errorModal();
+    } else {
+        abrirModal();
+        cerrarModal();
+    }
+    
+});
+
+const abrirModal = ()=> {
+    modal.innerHTML = `
+    <h2 class='modalBackground'> <p><i class="fa-solid fa-circle-check" style="color: #1cb698;"></i></p>Mensaje Enviado</h1>
+    `;
+}
+
+const errorModal = () => {
+    modal.innerHTML = `
+    <h2 class='modalBackground'> <p><i class="fa-solid fa-circle-exclamation" style="color: #1cb698;"></i></p>Error Revisa El Formulario</h1>
+    `;
+}
+
+const cerrarModal = ()=> {
+    setTimeout(function(){
+        modal.style.display = 'none'
+    }, 3000)
+}
+
+function validacion() {
+    let formValido = true;
+    let nombre = document.getElementById('name');
+
+    if(nombre === null || nombre.length < 5) {
+        formValido = false;
+    }
+
+    let numero = document.getElementById('numero');
+    if(isNaN(numero) || numero.length < 9){
+        formValido = false;
+    }
+
+    let mensaje = document.getElementById('mesagge');
+    if(mensaje === null || nombre.length < 5){
+        formValido = false;
+    } 
+
+    let email = document.getElementById('email');
+    if(email.length < 10) {
+        formValido = false;
+    }
+
+    return formValido;
+}
 
 let menuVisible = false;
 //Funcion que oculta o muestra el menu
@@ -14,7 +71,6 @@ function mostrarOcultarMenu(){
 
 function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
-
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
